5dc849d17cfac73683842ecb9cd912375adb1bd7
adospinoiu <alex.dospinoiu@gmail.com>
2022-11-15 22:22:08 -0700

    Working on the scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,25 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
         }
     },
 })
+
+/* ======== SCROLL SECTIONS ACTIVE LINK ========== */
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 58
+        const sectionId = current.getAttribute('id')
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId)
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
